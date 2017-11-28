6e58b66a2c8635ddf3ed4acd3d49cc1e0105e807
Capt.Liu <capt.liu@foxmail.com>
2017-11-28 17:28:35 +0800

    route learning

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+// 读取辅导员信息
+var instructor_info = require('../public/instructors.json')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '上海海事大学学生处“两随机一公开”系统' });
+});
+
+router.get('/data', function(req, res){
+  res.send(instructor_info);
 });
 
 module.exports = router;
